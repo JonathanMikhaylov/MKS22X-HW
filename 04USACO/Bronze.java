706bd747c5493156e3e62a9a54bab6b2988b8efe
JonathanMikhaylov <jonathan.mikhaylov@gmail.com>
2016-02-22 12:24:25 -0500

    Started Bronze.java

---
+++ 04USACO/Bronze.java
@@ -0,0 +1,22 @@
+public class Bronze() {
+   
+    public int R, C, E, N;
+    public int[][] g;
+
+    public static void main(String[] args){
+	
+    }
+
+    public setParams(String s) {
+	R = ;
+	C = ;
+	E = ;
+	N = ;
+	g = new int[][];
+    }
+
+    public calcVolume(int N) {
+
+    }
+
+}
